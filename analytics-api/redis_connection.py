ee09e7113c9b23d056393d4608aa9084ac4a4f23
diskind1 <menni.diskind@gmail.com>
2026-02-19 12:35:25 +0200

    commit 2 to api

--- analytics-api/redis_connection.py
+++ analytics-api/redis_connection.py
@@ -3,11 +3,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"),
-    port=int(os.getenv("REDIS_PORT", "6379")),
-    decode_responses=True,
-)
+def r_con():
+    r = redis.Redis(
+        host=os.getenv("REDIS_HOST", "redis"),
+        port=int(os.getenv("REDIS_PORT", "6379")),
+        decode_responses=True,
+    )
+    return r
 
 # CACHE_TTL = int(os.getenv("CACHE_TTL", "60"))
 
